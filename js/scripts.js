6f2c3398f06615b333a6e44332824128c243c744
Sankalp Shukla <snklpshkl14@gmail.com>
2020-10-25 14:11:31 +0530

    npm scripts 1

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+            $("#mycarousel").carousel( { interval: 2000 } );
+            $("#carouselButton").click(function(){
+                if($("#carouselButton").children("span").hasClass("fa-pause"))
+                {$("#mycarousel").carousel('pause');
+                 $("#carouselButton").children("span").removeClass("fa-pause");
+                 $("#carouselButton").children("span").addClass("fa-play")  
+                }
+                else if($("#carouselButton").children("span").hasClass("fa-play"))
+                {$("#mycarousel").carousel('cycle');
+                 $("#carouselButton").children("span").removeClass("fa-play");
+                 $("#carouselButton").children("span").addClass("fa-pause")  
+                }
+            });
+            $("#loginButton").click(function(){
+                $("#loginModal").modal('toggle');
+            });
+            $("#reserveButton").click(function(){
+                $("#reserveModal").modal('toggle');
+            });
+            $("#closeButton").click(function(){
+                $("#loginModal").modal('hide');
+            });
+            $("#cancelButton").click(function(){
+                $("#loginModal").modal('hide');
+            });
+            $("#closeReserve").click(function(){
+                $("#reserveModal").modal('hide');
+            });
+            $("#cancelReserve").click(function(){
+                $("#reserveModal").modal('hide');
+            });
+        });
